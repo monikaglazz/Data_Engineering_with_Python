c05c47da106bf81c0dcd1ffe2b2baefec2e47f5e
Monika Głaz <mosiex1998@gmail.com>
2023-02-16 11:47:38 +0100

    add chapter 2 and 3

--- Streamlined Data Ingestion with Pandas/chapter2.py
+++ Streamlined Data Ingestion with Pandas/chapter2.py
@@ -1,6 +1,9 @@
 # Load pandas as pd
 import pandas as pd
 
+# Load matplotlib as plt
+import matplotlib as plt
+
 
 #### SPREADSHEETS ####
 
@@ -16,3 +19,107 @@ survey_responses2 = pd.read_excel("fcc_survey_headers.xlsx",
                         usecols = col_string, 
                         skiprows = 2)
 
+# Create df from second worksheet by referencing its position
+responses_2017 = pd.read_excel("fcc_survey.xlsx",
+                               sheet_name = 1)
+
+# Create df from second worksheet by referencing its name
+responses_2017 = pd.read_excel("fcc_survey.xlsx",
+                               sheet_name = '2017')
+
+# Load both the 2016 and 2017 sheets by name
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name = ['2016','2017'])
+
+# Load all sheets in the Excel file
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name = [0, '2017'])
+
+# Load all sheets in the Excel file
+all_survey_data = pd.read_excel("fcc_survey.xlsx",
+                                sheet_name = None)
+
+
+# All sheets have been read into the ordered dictionary responses, 
+# where sheet names are keys and dataframes are values
+responses = {2016: survey_responses, 2017: responses_2017}
+
+# Create an empty dataframe
+all_responses = pd.DataFrame()
+
+# Set up for loop to iterate through values in responses
+for df in responses.values():
+  # Print the number of rows being added
+  print("Adding {} rows".format(df.shape[0]))
+  # Append df to all_responses, assign result
+  all_responses = all_responses.append(df)
+
+
+
+# Create string of lettered columns to load
+cols = 'AP,AH,AI,AK,AM'
+
+# Create dataframe from excel using only selected columns
+survey_data = pd.read_excel("fcc_survey.xlsx", 
+                            sheet_name = '2017',
+                            usecols = cols, 
+                            skiprows = 2)
+
+# Count NA values in each column
+print(survey_data.isna().sum())
+
+# Set dtype to load appropriate column(s) as Boolean data
+survey_data = pd.read_excel("fcc_survey_subset.xlsx",
+                            sheet_name = '2017',
+                            dtype={'HasDebt': bool})
+
+# View financial burdens by Boolean group
+print(survey_data.groupby('HasDebt').sum())
+
+# Set dtype to load appropriate column(s) as Boolean data
+survey_data = pd.read_excel("fcc_survey.xlsx", 
+                            usecols = cols, 
+                            sheet_name = '2017',
+                            skiprows = 2,
+                            dtype={'HasDebt': bool})
+
+# View financial burdens by Boolean group
+print(survey_data.groupby('HasDebt').sum())
+
+# Load file with Yes as a True value and No as a False value
+survey_subset = pd.read_excel("fcc_survey.xlsx",
+                            sheet_name = 'sub',
+                              dtype={"HasDebt": bool,
+                              "AttendedBootCampYesNo": bool},
+                              true_values = ["Yes"],
+                              false_values = ["No"])
+
+
+
+# Load file, with Part1StartTime parsed as datetime data
+survey_data = pd.read_excel("fcc_survey.xlsx",
+                            parse_dates=["Part1StartTime"])
+
+# Print first few values of Part1StartTime
+print(survey_data.Part1StartTime.head())
+
+# Create dict of columns to combine into new datetime column
+datetime_cols = {"Part2Start": ['Part2StartDate','Part2StartTime']}
+
+
+
+# Load file, supplying the dict to parse_dates
+survey_data = pd.read_excel("fcc_survey_dts.xlsx",
+                            parse_dates=datetime_cols)
+
+# View summary statistics about Part2Start
+print(survey_data.Part2Start.describe())
+
+
+# Parse datetimes and assign result back to Part2EndTime
+survey_data["Part2EndTime"] = pd.to_datetime(survey_data["Part2EndTime"],
+                                   format='%m%d%Y %H:%M:%S')
+
+
+# Print first few values of Part2EndTime
+print(survey_data.Part2EndTime.head())
---
+++ Streamlined Data Ingestion with Pandas/chapter3.py
@@ -0,0 +1,147 @@
+# Import sqlalchemy's create_engine() function
+from sqlalchemy import create_engine
+
+# Import pandas
+import pandas as pd
+
+# Create the database engine
+engine = create_engine('sqlite:///data.db')
+
+# Load hpd311calls without any SQL
+hpd_calls = pd.read_sql('hpd311calls', engine)
+
+# Create a SQL query to load the entire weather table
+query = """
+SELECT * 
+  FROM weather;
+"""
+
+# Load weather with the SQL query
+weather = pd.read_sql(query, engine)
+
+# Write query to get date, tmax, and tmin from weather
+query = """
+SELECT date, 
+       tmax, 
+       tmin
+  FROM weather;
+"""
+
+# Make a dataframe by passing query and engine to read_sql()
+temperatures = pd.read_sql(query, engine)
+
+# View the resulting dataframe
+print(temperatures)
+
+# Create query to get hpd311calls records about safety
+query = """
+SELECT *
+FROM hpd311calls
+WHERE complaint_type ='SAFETY';
+"""
+
+# Query the database and assign result to safety_calls
+safety_calls = pd.read_sql(query, engine)
+
+# Create query for records with max temps <= 32 or snow >= 1
+query = """
+SELECT *
+  FROM weather
+  WHERE tmax <= 32
+  OR snow >=1 ;
+"""
+
+# Query database and assign result to wintry_days
+wintry_days = pd.read_sql(query, engine)
+
+# View summary stats about the temperatures
+print(wintry_days.describe())
+
+# Create query for unique combinations of borough and complaint_type
+query = """
+SELECT DISTINCT borough, 
+       complaint_type
+  FROM hpd311calls;
+"""
+
+# Load results of query to a dataframe
+issues_and_boros = pd.read_sql(query, engine)
+
+# Check assumption about issues and boroughs
+print(issues_and_boros)
+
+# Create query to get call counts by complaint_type
+query = """
+SELECT complaint_type, 
+     COUNT(*)
+  FROM hpd311calls
+  GROUP BY complaint_type;
+"""
+
+# Create dataframe of call counts by issue
+calls_by_issue = pd.read_sql(query, engine)
+
+
+# Create query to get temperature and precipitation by month
+query = """
+SELECT month, 
+        MAX(tmax), 
+        MIN(tmin),
+        SUM(prcp)
+  FROM weather 
+ GROUP BY month;
+"""
+
+# Get dataframe of monthly weather stats
+weather_by_month = pd.read_sql(query, engine)
+
+# View weather stats by month
+print(weather_by_month)
+
+# Query to join weather to call records by date columns
+query = """
+SELECT * 
+  FROM hpd311calls
+  JOIN weather 
+  ON hpd311calls.created_date = weather.date;
+"""
+
+# Create dataframe of joined tables
+calls_with_weather = pd.read_sql(query, engine)
+
+# View the dataframe to make sure all columns were joined
+print(calls_with_weather.head())
+
+# Query to get water leak calls and daily precipitation
+query = """
+SELECT hpd311calls.*, weather.prcp
+  FROM hpd311calls
+  JOIN weather
+    ON hpd311calls.created_date = weather.date
+  WHERE hpd311calls.complaint_type = 'WATER LEAK';"""
+
+# Load query results into the leak_calls dataframe
+leak_calls = pd.read_sql(query, engine)
+
+# View the dataframe
+print(leak_calls.head())
+
+
+# Query to get heat/hot water call, tmax, tmin counts by created_date
+query = """
+SELECT hpd311calls.created_date, 
+	   COUNT(*), 
+       weather.tmax,
+       weather.tmin
+  FROM hpd311calls 
+       JOIN weather
+       ON hpd311calls.created_date = weather.date
+ WHERE hpd311calls.complaint_type = 'HEAT/HOT WATER' 
+ GROUP BY hpd311calls.created_date;
+ """
+
+# Query database and save results as df
+df = pd.read_sql(query, engine)
+
+# View first 5 records
+print(df.head())

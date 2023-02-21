1c45a237b8d74db0c1a4072d3eca60a613e2b430
Monika Głaz <mosiex1998@gmail.com>
2023-02-21 21:08:25 +0100

    update chapter1.py

--- Writing Efficient Python Code/Python Data Science Toolbox (Part 1)/chapter1.py
+++ Writing Efficient Python Code/Python Data Science Toolbox (Part 1)/chapter1.py
@@ -2,6 +2,8 @@
 import pandas as pd
 
 # Define shout with the parameter, word
+
+
 def shout(word):
     """Return a string with three exclamation marks"""
     # Concatenate the strings: shout_word
@@ -10,6 +12,7 @@ def shout(word):
     # Replace print with return
     return shout_word
 
+
 # Pass 'congratulations' to shout: yell
 yell = shout('congratulations')
 
@@ -17,19 +20,22 @@ yell = shout('congratulations')
 print(yell)
 
 # Define shout_all with parameters word1 and word2
+
+
 def shout_all(word1, word2):
-    
+
     # Concatenate word1 with '!!!': shout1
     shout1 = word1 + '!!!'
-    
+
     # Concatenate word2 with '!!!': shout2
     shout2 = word2 + '!!!'
-    
+
     # Construct a tuple with shout1 and shout2: shout_words
     shout_words = (shout1, shout2)
 
     # Return shout_words
     return shout_words
+
 
 # Pass 'congratulations' and 'you' to shout_all(): yell1, yell2
 yell1, yell2 = shout_all('congratulations', 'you')
@@ -51,7 +57,7 @@ col = df['lang']
 # Iterate over lang column in DataFrame
 for entry in col:
 
-    # If the language is in langs_count, add 1 
+    # If the language is in langs_count, add 1
     if entry in langs_count.keys():
         langs_count[entry] += 1
     # Else add the language to langs_count, set the value to 1
@@ -61,6 +67,7 @@ for entry in col:
 # Print the populated dictionary
 print(langs_count)
 
+
 # Define count_entries()
 def count_entries(df, col_name):
     """Return a dictionary with counts of 
@@ -68,16 +75,16 @@ def count_entries(df, col_name):
 
     # Initialize an empty dictionary: langs_count
     langs_count = {}
-    
+
     # Extract column from DataFrame: col
     col = df[col_name]
-    
+
     # Iterate over lang column in DataFrame
     for entry in col:
 
         # If the language is in langs_count, add 1
         if entry in langs_count.keys():
-            langs_count[entry] +=1
+            langs_count[entry] += 1
         # Else add the language to langs_count, set the value to 1
         else:
             langs_count[entry] = 1
@@ -85,8 +92,9 @@ def count_entries(df, col_name):
     # Return the langs_count dictionary
     return langs_count
 
+
 # Call count_entries(): result
-result = count_entries(tweets_df, 'lang')
+result = count_entries(df, 'lang')
 
 # Print the result
 print(result)

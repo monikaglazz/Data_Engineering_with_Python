54c18094f1d78421a34ac5236e94e567c696e7fe
Monika Głaz <mosiex1998@gmail.com>
2023-02-25 11:35:35 +0100

    create chapter1.sh

---
+++ Introduction to Shell/chapter1.sh
@@ -0,0 +1,51 @@
+# find out where you are in the filesystem
+
+pwd
+
+# lists the contents of your current directory
+
+ls
+
+# change current directory forward
+
+cd
+
+#  change current directory backwards
+
+cd ..
+
+# change directory to home
+
+cd ~
+
+# copy files
+
+cp original.txt duplicate.txt
+
+# copy files to different directory
+
+cp seasonal/autumn.csv seasonal/winter.csv backup
+
+# move files from one directory to other
+
+mv seasonal/summer.csv seasonal/spring.csv backup
+
+# move files from current directory up one level
+
+mv autumn.csv winter.csv ..
+
+# rename file
+
+mv old-winter.csv new-winter.csv
+
+# remove multiple files (permanently)
+
+rm autumn.csv seasonal/summer.csv
+
+# remove only empty directory
+
+rmdir people
+
+# create new empty directory
+
+mkdir yearly

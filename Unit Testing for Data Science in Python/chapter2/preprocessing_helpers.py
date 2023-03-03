e37314786a567c89e622c2988bbc9884a7cb038b
Monika Głaz <mosiex1998@gmail.com>
2023-03-03 10:02:30 +0100

    create preprocessing_helpers.py

---
+++ Unit Testing for Data Science in Python/chapter2/preprocessing_helpers.py
@@ -0,0 +1,23 @@
+def convert_to_int(integer_string_with_commas):
+    comma_separated_parts = integer_string_with_commas.split(",")
+    for i in range(len(comma_separated_parts)):
+        # Write an if statement for checking missing commas
+        if len(comma_separated_parts[i]) > 3:
+            return None
+        # Write the if statement for incorrectly placed commas
+        if i != 0 and len(comma_separated_parts[i]) != 3:
+            return None
+    integer_string_without_commas = "".join(comma_separated_parts)
+    try:
+        return int(integer_string_without_commas)
+    # Fill in with a ValueError
+    except ValueError:
+        return None
+
+
+def row_to_list(row):
+    row = row.rstrip("\\n")
+    separated_entries = row.split("\\t")
+    if len(separated_entries) == 2 and "" not in separated_entries:
+        return separated_entries
+    return None

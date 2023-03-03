dc5e5f10559bf03c161c183f6413053dfa6da345
Monika Głaz <mosiex1998@gmail.com>
2023-03-03 10:03:48 +0100

    create test_get_data_as_numpy_array.py

---
+++ Unit Testing for Data Science in Python/chapter2/test_get_data_as_numpy_array.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+from as_numpy import get_data_as_numpy_array
+
+
+def test_on_clean_file():
+    expected = np.array([[2081.0, 314942.0],
+                         [1059.0, 186606.0],
+                         [1148.0, 206186.0]
+                         ]
+                        )
+    actual = get_data_as_numpy_array("example_clean_data.txt", num_columns=2)
+    message = "Expected return value: {0}, Actual return value: {1}".format(
+        expected, actual)
+    assert actual == pytest.approx(expected), message
+
+# in the command line:
+# pytest test_get_data_as_numpy_array.py

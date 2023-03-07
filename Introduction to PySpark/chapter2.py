81cc7c8089fcd0b11131ef5c884321a5287d9925
Monika Głaz <mosiex1998@gmail.com>
2023-03-07 12:33:59 +0100

    create chapter2.py

---
+++ Introduction to PySpark/chapter2.py
@@ -0,0 +1,112 @@
+from spark_session import spark
+import pyspark.sql.functions as F
+from chapter1 import airports
+
+
+# ex 1 "Creating columns"
+# Create the DataFrame flights
+flights = spark.table("flights")
+
+# Show the head
+flights.show()
+
+# Add duration_hrs
+flights = flights.withColumn("duration_hrs", flights.air_time/60)
+
+
+# ex 2 "Filtering data"
+# Filter flights by passing a string
+long_flights1 = flights.filter("distance > 1000")
+
+# Filter flights by passing a column of boolean values
+long_flights2 = flights.filter(flights.distance > 1000)
+
+# Print the data to check they're equal
+long_flights1.show()
+long_flights2.show()
+
+
+# ex 3 "Selecting"
+# Select the first set of columns
+selected1 = flights.select("tailnum", "origin", "dest")
+
+# Select the second set of columns
+temp = flights.select(flights.origin, flights.dest, flights.carrier)
+
+# Define first filter
+filterA = flights.origin == "SEA"
+
+# Define second filter
+filterB = flights.dest == "PDX"
+
+# Filter the data, first by filterA then by filterB
+selected2 = temp.filter(filterA).filter(filterB)
+
+
+# ex 4 "Selecting with aliases"
+# Define avg_speed
+avg_speed = (flights.distance/(flights.air_time/60)).alias("avg_speed")
+
+# Select the correct columns
+speed1 = flights.select("origin", "dest", "tailnum", avg_speed)
+
+# Create the same table using a SQL expression
+speed2 = flights.selectExpr(
+    "origin", "dest", "tailnum", "distance/(air_time/60) as avg_speed")
+
+
+# ex 5 "Aggregating"
+# Find the shortest flight from PDX in terms of distance
+flights.filter(flights.origin == "PDX").groupBy().min("distance").show()
+
+# Find the longest flight from SEA in terms of air time
+flights.filter(flights.origin == "SEA").groupBy().max("air_time").show()
+
+
+# ex 6 "Aggregating II"
+# Average duration of Delta flights
+flights.filter(flights.origin == "SEA").filter(
+    flights.carrier == "DL").groupBy().avg("air_time").show()
+
+# Total hours in the air
+flights.withColumn("duration_hrs", flights.air_time /
+                   60).groupBy().sum("duration_hrs").show()
+
+
+# ex 7 "Grouping and Aggreagting"
+# Group by tailnum
+by_plane = flights.groupBy("tailnum")
+
+# Number of flights each plane made
+by_plane.count().show()
+
+# Group by origin
+by_origin = flights.groupBy("origin")
+
+# Average duration of flights from PDX and SEA
+by_origin.avg("air_time").show()
+
+
+# ex 8 "Grouping and Aggreagting II"
+# Group by month and dest
+by_month_dest = flights.groupBy("month", "dest")
+
+# Average departure delay by month and destination
+by_month_dest.avg("dep_delay").show()
+
+# Standard deviation of departure delay
+by_month_dest.agg(F.stddev("dep_delay")).show()
+
+
+# ex 9 ""
+# Examine the data
+print(airports.show())
+
+# Rename the faa column
+airports = airports.withColumnRenamed("faa", "dest")
+
+# Join the DataFrames
+flights_with_airports = flights.join(airports, on="dest", how="leftouter")
+
+# Examine the new DataFrame
+print(flights_with_airports.show())

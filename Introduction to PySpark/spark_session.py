7a64e27f00cf1375b7fbe8b61ff97c585359b106
Monika Głaz <mosiex1998@gmail.com>
2023-03-08 11:38:15 +0100

    create spark_session.py

--- Introduction to PySpark/spark_session.py
+++ Introduction to PySpark/spark_session.py
@@ -1,7 +1,18 @@
 from pyspark.sql import SparkSession
+from chapter1 import airports
 
 
 spark = SparkSession.builder.getOrCreate()
+
+# add table flights
 file_path = "/Data_Engineering_with_Python/Introduction to PySpark/flights.csv"
-table = airports = spark.read.csv(file_path, header=True)
-table.write.mode("overwrite").saveAsTable("flights")
+flights = spark.read.csv(file_path, header=True)
+flights.write.mode("overwrite").saveAsTable("flights")
+
+# add table planes
+file_path2 = "/Data_Engineering_with_Python/Introduction to PySpark/planes.csv"
+planes = spark.read.csv(file_path2, header=True)
+planes.write.mode("overwrite").saveAsTable("planes")
+
+# add taable airports
+airports.write.mode("overwrite").saveAsTable("airports")

1a51c4f802a60b7c563b273a5e8fa7f8d6426e5b
Monika Głaz <mosiex1998@gmail.com>
2023-03-15 16:45:22 +0100

    update chapter2.scala

--- Introduction to Scala/chapter2.scala
+++ Introduction to Scala/chapter2.scala
@@ -10,3 +10,77 @@ var handPlayerB: Int = kingHearts + jackHearts
 
 // Find and print the maximum hand value
 println(maxHand(handPlayerA, handPlayerB))
+
+
+// ex 2 "Create and parameterize an array"
+// Create and parameterize an array for a round of Twenty-One
+val hands: Array[Int] = new Array[Int](3)
+
+
+// ex 3 "Initialize an array"
+// Create and parameterize an array for a round of Twenty-One
+val hands: Array[Int] = new Array[Int](3)
+
+// Initialize the first player's hand in the array
+hands(0) = tenClubs + fourDiamonds
+
+// Initialize the second player's hand in the array
+hands(1) = nineSpades + nineHearts
+
+// Initialize the third player's hand in the array
+hands(3) = twoClubs + threeSpades
+
+
+// ex 4 "An array, all at once"
+// Create, parameterize, and initialize an array for a round of Twenty-One
+val hands = Array(tenClubs + fourDiamonds,
+              nineSpades + nineHearts,
+              twoClubs + threeSpades)
+
+
+
+// ex 5 "Updating array"
+// Initialize player's hand and print out hands before each player hits
+hands(0) = tenClubs + fourDiamonds
+hands(1) = nineSpades + nineHearts
+hands(2) = twoClubs + threeSpades
+hands.foreach(println)
+
+// Add 5♣ to the first player's hand
+hands(0) = hands(0) + fiveClubs
+
+// Add Q♠ to the second player's hand
+hands(1) = hands(1) + queenSpades
+
+// Add K♣ to the third player's hand
+hands(2) = hands(2) + kingClubs
+
+// Print out hands after each player hits
+hands.foreach(println)
+
+
+// ex 6 "Initialize and prepend to a list"
+// Initialize a list with an element for each round's prize
+val prizes = List(10,15,20,25,30)
+println(prizes)
+
+// Prepend to prizes to add another round and prize
+val newPrizes = 5 :: prizes
+println(newPrizes)
+
+
+
+//  ex 7 "Initialize a list using cons and Nil"
+// Initialize a list with an element each round's prize
+val prizes = 10 :: 15 :: 20 :: 25 :: 30 :: Nil
+println(prizes)
+
+
+
+//  ex 8 "Concatenate Lists"
+// The original NTOA and EuroTO venue lists
+val venuesNTOA = List("The Grand Ballroom", "Atlantis Casino", "Doug's House")
+val venuesEuroTO = "Five Seasons Hotel" :: "The Electric Unicorn" :: Nil
+
+// Concatenate the North American and European venues
+val venuesTOWorld = venuesNTOA ::: venuesEuroTO

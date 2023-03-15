1b87c590effb2960f3c63ef6ca41b9677b071cda
Monika Głaz <mosiex1998@gmail.com>
2023-03-15 12:20:56 +0100

    add chapter 3.scala

---
+++ Introduction to Scala/chapter2.scala
@@ -0,0 +1,12 @@
+// ex 1 "Call a function"
+def maxHand(handA: Int, handB: Int): Int = {
+  if (handA > handB) handA
+  else handB
+}
+
+// Calculate hand values
+var handPlayerA: Int = queenDiamonds + threeClubs + aceHearts + fiveSpades
+var handPlayerB: Int = kingHearts + jackHearts
+
+// Find and print the maximum hand value
+println(maxHand(handPlayerA, handPlayerB))
